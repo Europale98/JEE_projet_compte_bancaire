abb676e98f6bfd856cee9ba20884862e2eb82940
Flora Carré <flora.carre@gmail.com>
2021-02-28 19:09:32 +0100

    Diagrammes de séquence

--- src/main/sequences/UMLdiagram/MainUML.java
+++ src/main/sequences/UMLdiagram/MainUML.java
@@ -3,8 +3,14 @@ package UMLdiagram;
 public class MainUML {
 
     public static void main(String[] args) {
-        String sequence = CompteUML.CompteUML.creditUML();
-        RenderToPng.render(sequence, "diagrammes/sequences/credit");
+        RenderToPng.fileRender("diagrammes/sequences/credit.txt");
+        RenderToPng.fileRender("diagrammes/sequences/debitImpossible.txt");
+        RenderToPng.fileRender("diagrammes/sequences/creerClient.txt");
+        RenderToPng.fileRender("diagrammes/sequences/creerCompte.txt");
+        RenderToPng.fileRender("diagrammes/sequences/fermerCompte.txt");
+        RenderToPng.fileRender("diagrammes/sequences/suppressionClient.txt");
+        RenderToPng.fileRender("diagrammes/sequences/virement.txt");
+        RenderToPng.fileRender("diagrammes/sequences/virementInexistant.txt");
     }
 
 }
